9e69f0552be51c51784d65c6e71f853e999c47f2
Misael_Rodríguez <misaelrdz963@gmail.com>
2023-11-30 18:25:16 -0600

    npm start

--- back/app.js
+++ back/app.js
@@ -4,10 +4,11 @@ import cookieParser from 'cookie-parser';
 import productsRoutes from './routes/product.routes.js';
 import AuthRoutes from './routes/auth.routes.js';
 import emailRoutes from './routes/email.routes.js'
+import { FRONT_URL } from './config.js';
 import cors from 'cors';
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: FRONT_URL, credentials: true }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
