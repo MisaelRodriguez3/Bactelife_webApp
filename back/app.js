4ff8a0e94c470bca515bf6d5b6503f0564856d53
Misael_Rodríguez <misaelrdz963@gmail.com>
2023-12-04 16:58:09 -0600

    keep alive remove

--- back/app.js
+++ back/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
-import http from 'http'
 import productsRoutes from './routes/product.routes.js';
 import AuthRoutes from './routes/auth.routes.js';
 import emailRoutes from './routes/email.routes.js'
@@ -9,8 +8,6 @@ import { FRONT_URL } from './config.js';
 import cors from 'cors';
 
 const app = express();
-const server = http.createServer(app)
-server.keepAliveTimeout = 60000
 app.use(cors({ origin: FRONT_URL, credentials: true }));
 app.use(morgan('dev'));
 app.use(express.json());
