6d4aeadd989e34670ab92ce62c31e94619c6c78d
Misael_Rodríguez <misaelrdz963@gmail.com>
2023-12-04 13:46:28 -0600

    cookie options

--- back/controllers/auth.controller.js
+++ back/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import Auth from '../models/auth.model.js';
-import { ROOT, PASSWORD, SECRET_KEY, FRONT_URL } from '../config.js';
+import { ROOT, PASSWORD, SECRET_KEY, cookieOptions } from '../config.js';
 import bcrypt from 'bcryptjs'; //to encrypt the password
 import { createAccesToken } from '../libs/jwt.js';
 import jwt from 'jsonwebtoken';
@@ -12,12 +12,11 @@ export const register = async (req, res) => {
             user,
             password: passHash
         });
-
         const userSaved = await newUser.save();
         const token = await createAccesToken({ id: userSaved._id });
-        const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
         //res.cookie('token', token)
-        res.cookie('token', token, { httpOnly: true, secure: true, domain: FRONT_URL, partitioned: true, sameSite: 'none', expires: expirationDate });
+        res.cookie('token', token, cookieOptions);
         res.json('User created successfully');
     } catch (error) {
         console.log(error);
@@ -28,13 +27,12 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
     const { user, password } = req.body;
     try {
-        const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
 
         if (user === ROOT) {
             if (password === PASSWORD) {
                 const token = await createAccesToken({ user: 'root' });
                 // res.cookie('token', token);
-                res.cookie('token', token, { httpOnly: true, secure: true, domain: FRONT_URL, partitioned: true, sameSite: 'none', expires: expirationDate });
+                res.cookie('token', token, cookieOptions);
                 return res.json({ user: 'root' })
             } else { return res.status(400).json({ message: 'incorrect password' }); }
         }
@@ -45,7 +43,7 @@ export const login = async (req, res) => {
 
         const token = await createAccesToken({ id: userFound._id });
         //res.cookie('token', token)
-        res.cookie('token', token, { httpOnly: true, secure: true, domain: FRONT_URL, partitioned: true, sameSite: 'none', expires: expirationDate });
+        res.cookie('token', token, cookieOptions);
         res.json(userFound.user);
     } catch (error) {
         console.log(error);
