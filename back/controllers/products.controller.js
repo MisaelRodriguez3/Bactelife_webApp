362a5121f97041c7cdf0d223b48d0f418a0c515a
AlvaroDuron <duronalvaro974@gmail.com>
2023-12-07 16:56:28 -0600

    products controller modificado

--- back/controllers/products.controller.js
+++ back/controllers/products.controller.js
@@ -1,72 +1,71 @@
-import Product from '../models/products.model.js'
+import Product from "../models/products.model.js";
 
 export const addProduct = async (req, res) => {
-    const { name, description, price, pounds_per_yard, quarts_per_pound } = req.body;
-    try {
-        const newProduct = new Product({
-            name,
-            description,
-            price,
-            pounds_per_yard,
-            quarts_per_pound
-        });
-        await newProduct.save();
-        res.json(newProduct)
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'There was an internal server error' });
-    }
+  const { Gal_Product, description, price, pounds_per_yard, quarts_per_pound } =
+    req.body;
+  try {
+    const newProduct = new Product({
+      name,
+      description,
+      price,
+      pounds_per_yard,
+      quarts_per_pound,
+    });
+    await newProduct.save();
+    res.json(newProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "There was an internal server error" });
+  }
 };
 
 export const getProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.json(products)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: 'There was an internal server error' });
-    }
-
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "There was an internal server error" });
+  }
 };
 
 export const getProduct = async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        if (!product) {
-            return res.status(404).json({ error: 'this product does not exist' })
-        }
-        res.json(product);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'There was an internal server error' });
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ error: "this product does not exist" });
     }
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "There was an internal server error" });
+  }
 };
 
 export const updateProduct = async (req, res) => {
-    try {
-        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-            new: true
-        });
-        if (!product) {
-            return res.status(404).json({ error: 'this product does not exist' })
-        }
-        res.json(product);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'There was an internal server error' });
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!product) {
+      return res.status(404).json({ error: "this product does not exist" });
     }
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "There was an internal server error" });
+  }
 };
 
 export const deleteProduct = async (req, res) => {
-    try {
-        const product = await Product.findByIdAndDelete(req.params.id);
-        if (!product) {
-            return res.status(404).json('this product does not exist')
-        }
-        res.json('product deleted');
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'There was an internal server error' });
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      return res.status(404).json("this product does not exist");
     }
+    res.json("product deleted");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "There was an internal server error" });
+  }
 };
