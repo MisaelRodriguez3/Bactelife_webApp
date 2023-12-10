fbbac1d432f8f3d4bc91493cd77a41e77c2a652b
NihilXIX <140832464+NihilXIX@users.noreply.github.com>
2023-12-09 20:32:51 -0600

    ProductSelector.jsx

--- Bactelife_webApp/src/components/ProductSelector.jsx
+++ Bactelife_webApp/src/components/ProductSelector.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-import '../index.css';
+import '../index.css'
 
 export const ProductSelector = ({ products }) => {
     const [selectedProduct, setSelectedProduct] = useState('');
-    const [productInfo, setProductInfo] = useState(null); // Cambiado a null para representar objeto vacío
+    const [productInfo, setProductInfo] = useState('');
     const [selectedSize, setSelectedSize] = useState('');
     const [selectedUnit, setSelectedUnit] = useState('');
     const [selectedType, setSelectedType] = useState('');
     const [length, setLength] = useState('');
     const [width, setWidth] = useState('');
-    const area = length * width || 0; // Se añade verificación de valor numérico para el área
+    const area = length * width;
+
+    //formulas for the calculator
+    const estimatedCost = (((productInfo.pounds_per_yard * (selectedSize || area)) / selectedUnit) * productInfo.price).toFixed(2);
+    const productQuantity = ((productInfo.pounds_per_yard * (selectedSize || area)) / selectedUnit).toFixed(2);
+    const waterRequired = ((productInfo.quarts_per_pound * (selectedSize || area)) / selectedUnit).toFixed(2);
 
     const handleProductChange = (e) => {
         setSelectedProduct(e.target.value);
         const selectedProductInfo = products.find(product => product.name === e.target.value);
-        setProductInfo(selectedProductInfo || null); // Verifica si hay información del producto
+        setProductInfo(selectedProductInfo)
     };
 
     const handleSize = (e) => {
@@ -26,28 +31,22 @@ export const ProductSelector = ({ products }) => {
     };
 
     const handleType = (e) => {
-        setSelectedType(e.target.value);
+        setSelectedType(e.target.value)
         if (e.target.value === 'area') {
-            setWidth('');
-            setLength('');
+            setWidth('')
+            setLength('')
         } else {
-            setSelectedSize('');
+            setSelectedSize('')
         }
     };
 
     const handleLength = (e) => {
-        setLength(e.target.value);
+        setLength(e.target.value)
     };
 
     const handleWidth = (e) => {
-        setWidth(e.target.value);
+        setWidth(e.target.value)
     };
-
-    const price = selectedType === 'area' && productInfo ? productInfo.price_per_acre : (productInfo ? productInfo.price : 0); // Manejo del precio según el tipo de cálculo
-
-    const estimatedCost = ((productInfo && productInfo.cost_per_acre || 0) * (selectedType === 'area' ? area : (selectedSize || 1))) / selectedUnit * price || 0;
-    const productQuantity = (productInfo && productInfo.product_per_acre || 0) * (selectedType === 'area' ? area : (selectedSize || 1)) || 0;
-    const waterRequired = (productInfo && productInfo.water_per_acre || 0) * (selectedType === 'area' ? area : (selectedSize || 1)) / selectedUnit || 0;
 
     return (
         <div className="product">
@@ -81,18 +80,18 @@ export const ProductSelector = ({ products }) => {
             <select className="select" name="size" id="size" value={selectedUnit} onChange={handleUnit}>
                 <option value="" disabled>Select unit of measure</option>
                 <option value={1}>Yards</option>
-                <option value={8.3613e-5}>Hectares</option>
+                {selectedType === 'measures' ? '' : <option value={8.3613e-5}>Hectares</option>}
                 <option value={0.836127}>Meters</option>
                 <option value={8.3613e-7}>Kilometers</option>
             </select>
-            {productInfo ? <p className="price">Price: ${price}</p> : ''}
-            {(selectedProduct && selectedUnit && (selectedSize > 0 || area > 0)) && (
+            {selectedProduct ? <p className="price">Price: ${productInfo.price}</p> : ''}
+            {selectedProduct && selectedUnit && (selectedSize > 0 || area > 0) ?
                 <>
-                    <p className="estimated-cost">Estimated cost: ${estimatedCost.toFixed(2)}</p>
-                    <p className="product-quantity">Product quantity: {productQuantity.toFixed(2)} lb</p>
-                    <p className="water-required">Water required: {waterRequired.toFixed(2)} quarts</p>
-                </>
-            )}
+                    <p className="estimated-cost">Estimated cost: ${estimatedCost}</p>
+                    <p className="product-quantity">Product quantity: {productQuantity} lb</p>
+                    <p className="water-required">Water required: {waterRequired} quarts</p>
+                </> : ""
+            }
         </div>
     );
 };
