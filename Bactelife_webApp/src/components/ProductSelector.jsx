0b9a4682b1fd9aa0af51e54b5a6f567ec48caed6
osvaldo <osvaldoramos681@gmail.com>
2023-12-11 12:37:01 -0600

    Calculadora 3.0

--- Bactelife_webApp/src/components/ProductSelector.jsx
+++ Bactelife_webApp/src/components/ProductSelector.jsx
@@ -3,31 +3,26 @@ import '../index.css'
 
 export const ProductSelector = ({ products }) => {
     const [selectedProduct, setSelectedProduct] = useState('');
-    const [productInfo, setProductInfo] = useState({});
-    const [selectedSize, setSelectedSize] = useState('');
     const [selectedUnit, setSelectedUnit] = useState('');
-    const [selectedType, setSelectedType] = useState('');
     const [areaOption, setAreaOption] = useState('area');
     const [length, setLength] = useState('');
     const [width, setWidth] = useState('');
     const [area, setArea] = useState(null);
-    const [result, setResult] =useState(null);
+    const [result, setResult] = useState(null);
     const [costEstimate, setCostEstimate] = useState(null);
     const [costPerAcre, setCostPerAcre] = useState(null);
     const [waterEstimate, setWaterEstimate] = useState(null);
     const [productEstimate, setProductEstimate] = useState(null);
-
-    // const area = length * width;
 
     useEffect(() => {
         convertToAcres();
     }, [area, width, length, selectedUnit, areaOption]);
 
     useEffect(() => {
-        if(result !== null){
+        if (result !== null) {
             calculateCostEstimate();
         }
-        
+
     }, [result]);
 
     const convertToAcres = () => {
@@ -36,11 +31,12 @@ export const ProductSelector = ({ products }) => {
             hectares: 2.47105,
             meters: 0.000247105,
             kilometers: 247.105,
+            acre: 1
         };
 
-        if (areaOption ==='area'){
+        if (areaOption === 'area') {
             setResult(area * conversionFactors[selectedUnit]);
-        } else if (width && length){
+        } else if (width && length) {
             const areaM = width * length;
             setResult(areaM * conversionFactors[selectedUnit])
         }
@@ -49,33 +45,35 @@ export const ProductSelector = ({ products }) => {
 
     const calculateCostEstimate = () => {
         const gallonsPerAcre = 10;
-        const poundPerAcre = 0.75;
-        const discountRanges = [
-          { min: 0, max: 256, cost: 150 },
-          { min: 257, max: 2560, cost: 140 },
-          { min: 2561, max: 9999, cost: 135 },
-          { min: 10000, max: 24999, cost: 120 },
-          { min: 25000, max: 49999, cost: 110 },
-          { min: 50000, max: 99999, cost: 100 },
-          { min: 100000, max: 499999, cost: 80 },
-          { min: 500000, max: 999999, cost: 70 },
-          { min: 1000000, max: Infinity, cost: 60 },
-        ];
-    
+        const ozPerAcre = 12;
+
         const totalAcres = parseFloat(result);
-    
-        const discount = discountRanges.find(range => totalAcres >= range.min && totalAcres <= range.max);
-    
-        if (discount) {
-          setCostEstimate(discount.cost * totalAcres);
-          setCostPerAcre(discount.cost);
-            setWaterEstimate(gallonsPerAcre * totalAcres)
-            setProductEstimate(poundPerAcre * totalAcres)
+
+        let discountProduct = null;
+        let previousProduct = null;
+
+        for (const product of products) {
+            if (totalAcres < product.Acre) {
+                discountProduct = product;
+                break;
+            }
+            previousProduct = product;
+        }
+
+        if (!previousProduct && discountProduct) {
+            previousProduct = discountProduct;
+        }
+
+        if (previousProduct) {
+            setCostEstimate((previousProduct.Price * totalAcres).toFixed(2));
+            setCostPerAcre((previousProduct.Price).toFixed(2));
+            setWaterEstimate((gallonsPerAcre * totalAcres).toFixed(2))
+            setProductEstimate((ozPerAcre * totalAcres).toFixed(2))
 
         } else {
-          setCostEstimate(null); // No se encontró un descuento aplicable
-          setWaterEstimate(null);
-          setProductEstimate(null);
+            setCostEstimate(null); // No se encontró un descuento aplicable
+            setWaterEstimate(null);
+            setProductEstimate(null);
         }
     };
 
@@ -91,8 +89,6 @@ export const ProductSelector = ({ products }) => {
 
     const handleProductChange = (e) => {
         setSelectedProduct(e.target.value);
-        // const selectedProductInfo = products.find(product => product.name === e.target.value);
-        // setProductInfo(selectedProductInfo)
     };
 
     const handleSize = (e) => {
@@ -101,17 +97,6 @@ export const ProductSelector = ({ products }) => {
 
     const handleUnit = (e) => {
         setSelectedUnit(e.target.value);
-    };
-
-    const handleType = (e) => {
-        setSelectedType(e.target.value);
-        // Reset values when changing between 'area' and 'measures'
-        if (e.target.value === 'area') {
-            setWidth('');
-            setLength('');
-        } else {
-            setSelectedSize('');
-        }
     };
 
     const handleLength = (e) => {
@@ -128,10 +113,7 @@ export const ProductSelector = ({ products }) => {
             <br />
             <select className="select" name='products' value={selectedProduct} onChange={handleProductChange}>
                 <option value="" disabled>Select a product</option>
-                <option value="H2O Organix+Micrmin">H2O Organix+Micrmin</option>
-                {/* {products.map(product => (
-                    <option key={product._id} value={product.name}>{product.name}</option>
-                ))} */}
+                <option value="H2O Organix+Micrmin">H2O Organix+Micromin</option>
             </select>
             <br />
             <label className="label">Calculate by:</label>
@@ -160,14 +142,15 @@ export const ProductSelector = ({ products }) => {
                 <option value="hectares">Hectares</option>
                 <option value="meters">Meters</option>
                 <option value="kilometers">Kilometers</option>
+                <option value="acre">Acres</option>
             </select>
-            
-            {selectedProduct ? <p className="price">Price per Acre: ${costPerAcre}</p> : ''}
+
             {selectedProduct && selectedUnit && (result > 0) ?
                 <>
+                    <p className="price">Price per Acre: ${costPerAcre}</p>
                     <p className="acres">Acre(s): {result}</p>
                     <p className="estimated-cost">Estimated cost: ${costEstimate}</p>
-                    <p className="product-quantity">Product quantity: {productEstimate} lb</p>
+                    <p className="product-quantity">Product quantity: {productEstimate} Oz</p>
                     <p className="water-required">Water required: {waterEstimate} gallons</p>
                 </> : ""
             }
